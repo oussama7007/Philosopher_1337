b5c4ac5781c6d1f0e28c51d024b0c8ba74173552
oussama7007 <oussamatachakourt@gmail.com>
2025-04-20 10:41:16 +0100

    DD

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -9,6 +9,11 @@
 #include <string.h>
 #include <pthread.h>
 
-
-
+// typedef struct s_philo
+// {
+//     unsigned int       id;
+//     unsigned int       meals_eaten;
+//     pthread_mutex_t    l_fork;
+//     pthread_mutex_t     r_fork;
+// } t_philo;
 #endif 
