254f232ff0aa695671222588f0c43c3e84b8c265
oussama7007 <oussamatachakourt@gmail.com>
2025-04-21 17:51:27 +0100

    done

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 10:38:30 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/04/21 12:35:34 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/04/21 17:49:23 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philosopher.h"
-int is_valid(long n)
+#include "../includes/philosopher.h"
+int is_valid(unsigned int n)
 {
-    if(n <= 0 || n >2147483647)
+    if(n == 0 ||  n > 2147483647)
         return(-1);
     return 1;
 }
@@ -22,71 +22,96 @@ int is_digit(char *str)
     int i;
 
     i = 0;
-    while(str[i])
+    while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if(str[i] == '-' || str[i] == '+')
     {
-        if(str[i] >= '9' && str[i] <= '0')
+        if(str[i] == '-')
             return -1;
         i++;
     }
+    while(str[i] && !(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
+    {
+        if(str[i] > '9' || str[i] < '0')
+            return -1;
+        i++;
+    }
+    while(str[i] && (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)))
+        i++;
+    if(str[i] != '\0')
+        return -1;
+ 
     return 1;
 }
-long	ft_atol(const char *str)
+unsigned int	ft_atou(const char *str)
 {
-	long	res;
-	int		sign;
+	unsigned int 	res;
 	int		i;
 
 	res = 0;
 	i = 0;
-	sign = 1;
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
+	if (str[i] == '+')
 		i++;
-	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = res * 10 + (str[i] - '0');
 		i++;
 	}
-	return (res * sign);
+	return (res);
 }
 int check_args(int ac, char **av)
 {
     int i;
-    int n;
       
     i = 0;
-    while(i++ < ac)
+    while(++i < ac)
     {
-        if(!is_digit(av[i]))
+       
+        if(is_digit(av[i]) != 1)
             return -1;
-        n = ft_atol(av[i]);
-        if(!is_valid(n))
+        if(is_valid(ft_atou(av[i])) != 1)
             return -1;
+        
     }
+    return 1;
 }
 void ft_putstr_fd(char *str, int fd)
 {
     int i;
 
-    i = -1;
-    while(str[i++])
+    i = 0;
+    while(str[i])
+    {
         write(fd, &str[i], 1);
+        i++;
+        
+    }
 }
+int     init_program(t_prosses *program, int ac, char **av)
+{
+        program->N_philos = ft_atou(av[1]);
+        program->T_die = ft_atou(av[2]);
+        program->T_eat = ft_atou(av[3]);
+        program->T_sleep = ft_atou(av[4]);
+        if(ac == 5)
+            program->N_must_eat = -1;
+        else 
+            program->N_must_eat = ft_atou(av[4]);
+        
+        
 
+}
 int main(int ac, char **av)
 {
-    t_philo philo;
-    pthread_t master;
+     t_prosses philo;
+     pthread_t master;
     
     if(ac < 5 || ac > 6)
         return(ft_putstr_fd("Error: args must be 4 OR 5\n", 2), 1);
-    if(!check_args(ac , av))
+    if(check_args(ac , av) != 1)
         return(ft_putstr_fd("Error: invalid arg \n", 2), 1);
-    init_program(&philo)
+    init_program(&philo, ac, av);
     
 }
