fea293409228978d0fc378b91c4d63a7623cdc92
oussama7007 <oussamatachakourt@gmail.com>
2025-04-21 22:21:04 +0100

    done

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -7,12 +7,22 @@
 #include <string.h>
 #include <pthread.h>
 
-typedef struct s_philo
+// typedef struct s_philo
+// {
+//     unsigned int       id;
+//     unsigned int       meals_eaten;
+
+//     pthread_mutex_t    l_fork;
+//     pthread_mutex_t     r_fork;
+// } t_philo;
+
+typedef struct s_prosses
 {
-    unsigned int       id;
-    unsigned int       meals_eaten;
-    
-    pthread_mutex_t    l_fork;
-    pthread_mutex_t     r_fork;
-} t_philo;
+    unsigned int N_philos;
+    unsigned int T_die;
+    unsigned int T_eat;
+    unsigned int T_sleep;
+    unsigned int N_must_eat;
+} t_prosses;
+
 #endif 
