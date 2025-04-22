8656632f8223c71236f0fc3afc08f9fe39737317
oussama7007 <oussamatachakourt@gmail.com>
2025-04-22 16:16:15 +0100

    done

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -6,15 +6,17 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
-#
+
 typedef struct s_philo
 {
-    unsigned int       id;
-    unsigned int       meals_eaten;
-
-    pthread_mutex_t    l_fork;
-    pthread_mutex_t     r_fork;
-} t_philo;
+	// unsigned int 					id;
+	// unsigned int 					meals_eaten;
+	// size_t				last_meal;
+	// pthread_t			thread;
+	// struct s_prosses	*program;
+	// pthread_mutex_t		*r_fork;
+	// pthread_mutex_t		*l_fork;
+}		t_philo ;
 
 typedef struct s_prosses
 {
@@ -23,7 +25,8 @@ typedef struct s_prosses
     unsigned int T_eat;
     unsigned int T_sleep;
     unsigned int N_must_eat;
-    struct t_philo *philos
+    pthread_mutex_t  *fork;
+    t_philo *philos;
 } t_prosses;
 
 #endif 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 10:38:30 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/04/21 22:32:22 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/04/22 13:53:10 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,7 @@ void ft_putstr_fd(char *str, int fd)
         
     }
 }
-int     init_program(t_prosses *program, int ac, char **av)
+int     assigning_values(t_prosses *program, int ac, char **av)
 {
         program->N_philos = ft_atou(av[1]);
         program->T_die = ft_atou(av[2]);
@@ -101,6 +101,16 @@ int     init_program(t_prosses *program, int ac, char **av)
             program->N_must_eat = ft_atou(av[4]);
 
 }
+int     allocate_resources(t_prosses *philo)
+{
+    philo->fork = malloc(sizeof(pthread_mutex_t) * philo->N_philos);
+    if(!philo->fork)
+        return -1;
+    philo->philos = malloc(sizeof(t_philo) * philo->N_philos);
+    if(!philo->philos)
+        return -1;
+        
+}
 int main(int ac, char **av)
 {
      t_prosses philo;
@@ -110,6 +120,8 @@ int main(int ac, char **av)
         return(ft_putstr_fd("Error: args must be 4 OR 5\n", 2), 1);
     if(check_args(ac , av) != 1)
         return(ft_putstr_fd("Error: invalid arg \n", 2), 1);
-    init_program(&philo, ac, av);
+    assigning_values(&philo, ac, av);
+    if(allocate_resources(&philo) != 1)
+        return(ft_putstr_fd("Error: allocation failed\n", 2), 1);
     
 }
