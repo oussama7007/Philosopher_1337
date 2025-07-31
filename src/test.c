768dc305e7a0dcb92a3d22359e009d4b50f218cb
oussama7007 <oussamatachakourt@gmail.com>
2025-07-31 10:06:18 +0100

    still bonus and make sure mandatory works correctly

--- src/test.c
+++ src/test.c
@@ -5,6 +5,7 @@
 # include <pthread.h>
 # include <stdlib.h>
 # include <sys/time.h>
+#include <unistd.h>
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -19,18 +20,41 @@ void    *function(void *arg)
     printf("Add number = %ld\n", *add_number);
     return NULL;
 }
+long long gt_time(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
 int main()
 {
-    pthread_mutex_t lock;
-    pthread_mutex_init(&lock, NULL);
-    pthread_mutex_lock(&lock);
-    printf("%d", 5555);
-int ret1 = pthread_mutex_unlock(&lock);
-printf("\nret1 = %d (%s)\n", ret1, strerror(ret1));
+    // pthread_t thread1 , thread2;
+    // long long value = 8;
+    // long long value2 = 20;
+    // pthread_create(&thread1, NULL, function, &value);
+    // pthread_create(&thread2, NULL, function, &value2);
+   
+    
+    
+    while (true)
+    {
+        long long time  = gt_time();
+        usleep(1000);
+        long long value = gt_time() - time + 0.01;
+        if(value != 0)
+            printf("error and value = %lld \n", value);
+        else 
+            printf("correct\n");
+    }
+    // pthread_join(thread1, NULL);
+    // pthread_join(thread2, NULL);
 
-int ret2 = pthread_mutex_unlock(&lock);
-printf("ret2 = %d (%s)\n", ret2, strerror(ret2));
-
-int ret3 = pthread_mutex_unlock(&lock);
-printf("ret3 = %d (%s)\n", ret3, strerror(ret3));
+    // pthread_mutex_t lock;
+    // pthread_mutex_init(&lock, NULL);
+    // pthread_mutex_lock(&lock);
+    // printf("%d", 5555);
 }
+
+
+
+
